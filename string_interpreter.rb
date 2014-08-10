1aea53671bb7db38e322c8cb781eb84c02e5a3d9
msparkman <mhsparkman@gmail.com>
2014-08-10 12:57:56 -0500

    String Interpreter
    Adding what I have so far for my string interpreter. This will be for
    messing around with Ruby and unit tests.

---
+++ string_interpreter.rb
@@ -0,0 +1,65 @@
+class StringInterpreter
+	attr_accessor :word
+
+	# Initialize the variable if one is not given upon object creation
+	def initialize(word = "hello")
+		@word = word
+	end
+
+	# check if the word is a palindrome
+	def palindrome?
+		if @word.nil?
+			puts "word is nil"
+		elsif @word.empty?
+			puts "word is empty"
+		else
+			if @word.respond_to? "downcase"
+				@word = @word.downcase
+				if @word.respond_to? "scan"
+					@word = @word.scan /\w/
+					if @word.respond_to? "reverse"
+						puts @word == @word.reverse
+					end
+				end
+			end
+			
+		end
+	end
+	# Recursive alternative
+	#def palindrome?
+	#	if @word.nil?
+	#		false
+	#	elsif @word.length == 1 || @word.length == 0
+	#		true
+	#	else
+	#		if @word[0] == @word[-1]
+	#			palindrome?(@word[1..-2])
+	#		else
+	#			false
+	#		end
+	#	end
+	#end
+	
+	#def 
+end
+
+if __FILE__ == $0
+	wordAnalyzer = WordAnalyzer.new "goodbye"
+	wordAnalyzer.palindrome?
+
+	# Change word to be a palindrome
+	wordAnalyzer.word = "Racecar"
+	wordAnalyzer.palindrome?
+
+	# Change to a palindromic sentence
+	wordAnalyzer.word = "Never a foot too far, even."
+	wordAnalyzer.palindrome?
+	
+	# Change the word to an empty string
+	wordAnalyzer.word = ""
+	wordAnalyzer.palindrome?
+  
+	# Change to nil
+	wordAnalyzer.word = nil
+	wordAnalyzer.palindrome?
+end
---
+++ test_string_interpreter.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require 'string_interpreter'
+
+class TestStringInterpreter < Test::Unit::TestCase
+    def test_palindrome
+    	#test the palidrome method
+    end
+end
